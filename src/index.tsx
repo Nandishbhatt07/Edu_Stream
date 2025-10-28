4f338e7db393c54e7d54cf7272669fcc456ef0a0
Nandish Bhatt <122980004+Nandishbhatt07@users.noreply.github.com>
2025-10-28 18:22:19 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; // This imports your global styles
-import App from './App.tsx'; // This imports your main App component
+import App from './App'; // This imports your main App component
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
